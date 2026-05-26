ab9a21f21577bdcb27c3b17dc22eb5f14ab69e2f
dkzentharis-dot <dkzentharis@gmail.com>
2026-05-26 23:18:59 +0530

    Update defaultCards.js

--- src/constants/defaultCards.js
+++ src/constants/defaultCards.js
@@ -6,7 +6,7 @@ export const defaultCards = [
     preview: "Not just a cap… a quiet piece of your care, Maa",
     description: `“Maa… 🤍 This cap may look simple to others, but to me it feels like your care quietly staying with me every single day. When the sunlight becomes too harsh, it shades me gently like the way your words calm my mind, and when unexpected rain falls, it reminds me that some comforts arrive silently… just like you 🌧️☀️. It stays above me without asking for attention, protecting me in every season, and somehow our friendship feels the same — soft, silent, but always present when I need it the most. Maybe that’s why this gift means so much to me, because it is not just a cap anymore; it carries your thought, your kindness, and the warmth you give me without even trying 🥹✨. Every time I wear it, it feels like a small piece of you is walking beside me, protecting my peace, making me smile, and reminding me that some friendships are not just memories… they become comfort, care, and home itself 🤍”
 `,
-    image: "https://drive.google.com/file/d/1XCnUJRPqF-C9saq40dnC8-O8aP216HOf/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1gj53LaKbPBVA5pkL_gs3DNqzz58d4_PW/view?usp=drive_link"
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ export const defaultCards = [
     preview: "Not just a ring…",
     description: `This ring may look small to others, but for me, it carries something far bigger than its size 🤍. I was afraid that if I wore it on my finger, one careless moment might make me lose it somewhere in life… so I placed it on a chain and kept it closer to my heart, where it truly belongs. Because this is not just a ring anymore — it became a part of my days, my memories, my comfort, and our friendship itself 🌙. In silent moments it reminds me of your care, in difficult moments it feels like strength, and in happy moments it quietly becomes the reason behind my smile. Some gifts stay in hands for a while, but yours found a place inside my heart forever, Maa ✨
 `,
-    image: "https://drive.google.com/file/d/1mgdhTdmmeglenFGF6s6s4O8K2caJGUFV/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1wpGAIMn8IWFMPPJW_KIOws60dEDcjBOx/view?usp=sharing"
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ export const defaultCards = [
     preview: "A tiny heart-shaped gift…",
     description: `This tiny heart-shaped keychain may look soft and simple to others, but to me, it carries something much deeper than just a gift 🤍 The way it rests inside the warm light, quietly glowing in that cupboard, feels exactly like our friendship — calm, rare, and beautifully comforting. Not loud, not temporary… just a bond that slowly became a safe place in my life, Maa ✨ The softness of the heart reminds me of your care, the way you understand even the unspoken things, and how your presence turns ordinary moments into memories I never want to lose 🥹 Some friendships are only conversations, but ours feels like a small world built with trust, comfort, protection, laughter, and silent support 🌙 And this little keychain somehow holds all of that inside it. It’s not just a heart-shaped gift anymore… it became a symbol of a friendship I’ll always keep close to me, like something precious that deserves its own light forever 💫
 `,
-    image: "https://drive.google.com/file/d/1GN_hrxivpwAtKVxKTHEqvXIq5sLhWxR_/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1RFkGvt7CHgCSiaoUW3p0B4anhx9JEFEk/view?usp=drive_link"
   },
   {
     id: 4,
@@ -31,7 +31,7 @@ export const defaultCards = [
     description: `This cup was never just a gift to me, Maa 🤍
 Whenever my health feels weak or my mind feels tired, the tea I make in this cup somehow brings a different kind of comfort — not because of the tea alone, but because it carries your care within it.Every small sip reminds me that our friendship is not built only on words, but on the quiet ways we look after each other.In my hardest moments, this simple cup became a feeling of warmth, support, and peace… like a small part of you staying beside me, silently reminding me that I am never alone, Paa ✨
 `,
-    image: "https://drive.google.com/file/d/1Aq_Frid_GXDpoXXw6Sq8_7OXpI0sz1XK/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1Ee_-722b6V_YdeaHYXXOzLkIAdXedgma/view?usp=drive_link"
   },
   {
     id: 5,
@@ -39,7 +39,7 @@ Whenever my health feels weak or my mind feels tired, the tea I make in this cup
     preview: "Your fragrance stayed… so did our friendship 🩵",
     description: `The sandal fragrance you gave me is no longer just a room spray, Maa… 🤍 Whenever its scent slowly spreads through my room, it feels like your care quietly reaches every corner of my heart too. Even in your absence, that soft sandal aroma gives me the comfort of your presence — like you are still near me, sitting beside me, protecting my peace in silence 🌙 Some fragrances disappear after a while… but this one stays like our friendship — calm, pure, and unforgettable. Every time I breathe in that scent, it reminds me of the bond we carry: not something seen every day, but something deeply felt in every lonely moment ✨ The room may be empty, but your fragrance turns it into a place filled with memories, warmth, and friendship. Just like sandal stays long after it is applied, your care stays with me long after every conversation ends, Maa 🕊️
 `,
-    image: "https://drive.google.com/file/d/1_VP0x79DXmv5id11tI8kn4EgpFe4BfBk/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/11GTepNAqXP0B6RU6Gf1v4V3K6CRx85mg/view?usp=drive_link"
   },
   {
     id: 6,
@@ -49,7 +49,7 @@ Whenever my health feels weak or my mind feels tired, the tea I make in this cup
 
 Because our bond was never built only through conversations or meetings… it lives even in the smallest things.This fragrance became a silent memory of us — reminding me that no matter how far life goes, your friendship will always stay close to my heart like a comforting presence beside me, Paa & Maa forever ✨
 `,
-    image: "https://drive.google.com/file/d/1NxWg9vg2tXrx63U3GmfajImChYs_5-8i/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1p6dS-PI9xrSTcIYUyNlD5nXIn76DBgmY/view?usp=drive_link"
   },
   {
     id: 7,
@@ -57,7 +57,7 @@ Because our bond was never built only through conversations or meetings… it li
     preview: "Your care feels like peace on my forehead, Maa🩵",
     description: `Every time I take this sandal paste and place it on my forehead, it is never just for relief from a headache or tiredness… somehow it feels like a calm blessing resting over me 🤍 The fragrance carries a strange peace — like God quietly sending comfort through memories, and in that small moment, I feel your friendship beside me, Maa ✨ Because some people heal with medicines… but rare souls heal hearts simply through their presence. And your care became one of those peaceful feelings I carry every day 🌙
 `,
-    image: "https://drive.google.com/file/d/1vWpFepu662SA9u3161IKi3v1CKRvlOwU/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1-KdrzsygMmj8AR9AgfI2E3X6X73do7Lp/view?usp=drive_link"
   },
   {
     id: 8,
@@ -65,7 +65,7 @@ Because our bond was never built only through conversations or meetings… it li
     preview: "Not just my first car…",
     description: `This keychain was never just a small gift, Maa… 🤍 The moment you gave it to me, it quietly carried our entire friendship inside it — the memories, the care, the comfort, and the feeling of always having someone beside me. The childhood photo inside it makes it even more special, because it holds the little version of us who once shared innocent smiles and a pure bond that even time could never erase. And now, when I hold the keys of my very first car, this keychain becomes the most important part of it — not because it hangs beside the key, but because your presence travels with me in every ride 🚘✨ In the silence of night drives, the glow of the dashboard, and every road ahead, this little keychain reminds me that some friendships are not temporary moments… they become a part of life itself. Others may only see a car key, but for me, it is a symbol of us — a friendship that stayed in my heart long before this car ever reached my hands. My first car may mark a new journey in life, but your gift is what makes the journey feel complete, Maa 🤍
 `,
-    image: "https://drive.google.com/file/d/1Em9iKFv2aEaCMVICAbZ0hbh1BSzQqd68/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1sNo6-DSxJK8SrzIEeWzQNRqL0kTtJ3Sf/view?usp=drive_link"
   },
   {
     id: 9,
@@ -73,7 +73,7 @@ Because our bond was never built only through conversations or meetings… it li
     preview: "Protected inside your heart… forever, Maa",
     description: `This chain may show “Maa inside Paa”… but the real meaning is far deeper than words 🤍 It feels like Maa is quietly protecting Paa from inside her heart, holding him close through every silence, every care, and every memory we created together. This chain is not just an ornament or a simple gift; it feels like an invisible promise connecting two souls through the purest form of friendship ✨ Some bonds in this world are ordinary and temporary, but ours feels rare — something that cannot be copied, recreated, or replaced by anyone else. Because this was never made with material value alone; it was created with emotion, trust, comfort, protection, and the peaceful connection we became for each other 🌙 Whenever this chain rests near my heart, it reminds me that no matter where life takes us, our friendship will always stay connected — like Maa quietly protecting Paa forever, with love, care, and a bond that time itself could never break 🤍
 `,
-    image: "https://drive.google.com/file/d/1TItJbPTESMq2urVXnnmPFRo7yvDl6sWj/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1o1F-o9oBXoLOYJxAWKm8ppxC8Z4KeeI1/view?usp=drive_link"
   },
   {
     id: 10,
@@ -81,7 +81,7 @@ Because our bond was never built only through conversations or meetings… it li
     preview: "A bond where friendship quietly became protection",
     description: `You became the cutest little album of our story, Maa — a place where nine years of not seeing each other quietly turned into four months of the purest and healthiest bond my heart has ever known 🤍 Whenever I keep that album inside my cupboard and look at it under that warm light, it never feels like an ordinary memory… it feels like a treasure I searched for my entire life and finally found. Every page carries a silent emotion — the childhood we lost, the smiles we found again, the comfort we became for each other, and the friendship that healed parts of us without even asking. Some people leave memories behind, but you left a feeling that stays alive in every corner of my life, Maa ✨ That cupboard is not just holding an album — it is protecting a bond that survived distance, silence, years, and fate itself. Because what we have is not something temporary or ordinary; it is the kind of pure friendship the universe creates only once in a lifetime 🌙 A normal friendship gives memories, but ours slowly became something deeper — a bond where I wished to give you the warmth, protection, encouragement, and reassurance that life once took away too early. I may never replace the father you lost in childhood, Maa… but from my heart, I always wanted to become the safe place that gives you freedom, care, peace, support, and a shoulder where your heart never feels alone again 🤍 If the world ever becomes heavy for you, Paa will always stand beside you quietly — protecting your smile, supporting your dreams, listening to your silence, and reminding you that no matter how difficult life becomes, you will always have someone who truly cares for you beyond words ✨ And whenever I stand there and look at those memories, my heart quietly says: “Out of everything life gave me… meeting you again was the most beautiful treasure of all.” 🤍
 .`,
-    image: "https://drive.google.com/file/d/1cGLyqCfKkSWgO4EcUKLghTm9i0sxdnch/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1Jk3WsxftNaecfXk74I2fSrL6Qju0CQDB/view?usp=drive_link"
   },
   {
     id: 11,
@@ -93,6 +93,6 @@ Even during the years we never met, I never forgot your kindness, your character
 
 The gifts you gave me were never just things, Maa… they were feelings filled with your love, effort, thoughts, and heart 🥹 After nine years of emptiness, your care reminded me that I mattered to someone again.Every little thing you gave became a treasure to me not because of its value, but because it came from you.You unknowingly filled the silence life left inside me and became a comfort I never thought I would receive again.No matter how much time passes, this room, this art, and this friendship will always remind me of one truth — some souls are sent back into our lives not just to stay for a season, but to become peace itself 🤍
 `,
-    image: "https://drive.google.com/file/d/1TX1PZhWqeWEVn57tRtrpUi8KSbA7Jw69/view?usp=drive_link"
+    image: "https://drive.google.com/file/d/1GJTrKqH7XkDlB-JLsZiEpk_381tJfATB/view?usp=drive_link"
   }
 ];
